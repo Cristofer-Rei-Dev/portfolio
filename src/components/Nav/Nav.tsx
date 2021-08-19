65151b75c5a524bee5229d31878451f1319c49b7
Cristofer Portela <cristoferportela@gmail.com>
2021-08-19 12:09:59 -0300

    feat: navigator of the site

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,84 @@
 import React from 'react'
+import { MainHeader } from '../Styled/Header'
+import { Logo } from '../Styled/Images'
+// @ts-ignore
+import logoImg from '../../images/rei-dev-white.webp'
+import { Row, RowItem } from '../Styled/Container'
+import { Text } from '../Styled/Text'
+import StyledTheme from '../Styled'
 
+
+/**
+ * * Will render the main navigation tab (top of page)
+ * 
+ * @returns {React.FunctionComponent}
+ */
 const Nav = () => {
     return (
-        <div>
-            
-        </div>
+        <MainHeader>
+            <Row style={{ padding: "0 15px" }} as="article" items={"center"} content={"space-evenly"}>
+                <Row as={RowItem} spacing={4} items={"center"}>
+                    <Logo
+                        src={logoImg}
+                        alt={"Rei dev"}
+                    />
+                    <Text className="border-left">
+                        Desenvolvimento
+                        <br />
+                        Profissional
+                    </Text>
+                </Row>
+                <Row as={RowItem} spacing={4} items={"center"}>
+                    <Row as="ul" content={"space-evenly"}>
+                        <li>
+                            Lorem ipsum
+                        </li>
+                        <li>
+                            Lorem ipsum
+                        </li>
+                        <li>
+                            Lorem ipsum
+                        </li>
+                    </Row>
+                </Row>
+                <Row as={RowItem} spacing={3}>
+                    <Row as="ul" content={"flex-start"}>
+                        <li>
+                            <button>
+                                Cadastrar
+                            </button>
+                        </li>
+                        <li>
+                            <button>
+                                Login
+                            </button>
+                        </li>
+                    </Row>
+                </Row>
+            </Row>
+            <Row style={{ padding: "8px 15px" }} as="article" items={"center"} content={"center"}>
+                <Row as={"ul"} content={"space-evenly"}>
+                    <li>
+                        Lorem ipsum
+                    </li>
+                    <li>
+                        Lorem ipsum
+                    </li>
+                    <li>
+                        Lorem ipsum
+                    </li>
+                    <li>
+                        Lorem ipsum
+                    </li>
+                    <li>
+                        Lorem ipsum
+                    </li>
+                    <li>
+                        Lorem ipsum
+                    </li>
+                </Row>
+            </Row>
+        </MainHeader>
     )
 }
 
