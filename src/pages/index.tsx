97ee5c645f8ba1a7a7f638f05936141db4ae52e6
Cristofer Portela <cristoferportela@gmail.com>
2021-08-19 12:11:19 -0300

    feat: added theme provider and global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import * as React from "react"
+import { ThemeProvider } from "styled-components"
+import Nav from "../components/Nav/Nav"
+import StyledTheme, { Global } from "../components/Styled"
+import '../sass/main.sass'
 
 
 const IndexPage = () => {
   return (
-    <main >
+    <main>
+      <ThemeProvider theme={StyledTheme}>
+        <Global />
+        <Nav />
+      </ThemeProvider>
     </main>
   )
 }
