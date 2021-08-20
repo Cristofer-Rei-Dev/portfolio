818d23fbc7ba82f71b5bfede7a100a72b2a72c92
Cristofer Portela <cristoferportela@gmail.com>
2021-08-20 01:36:23 -0300

    feat: text align added

--- src/components/Styled/Text.tsx
+++ src/components/Styled/Text.tsx
@@ -7,13 +7,16 @@ interface iText {
     size?: string
     weight?: number
     decoration?: string
+    align?: string
 }
 
 export const Text = styled.p<iText>`
-    font-family: Helvetica, Arial, sans-serif;
-    ${(props) => props.color ? `color: ${props.theme[props.color]};` : ''}
+    font-family: Roboto, Helvetica, Arial, sans-serif;
     font-size: ${(props) => props.size ? props.size : '1rem'};
     font-weight: ${(props) => props.weight ? props.weight : '500'};
+    
+    ${(props) => props.align ? `text-align: ${props.align};` : ''}
+    ${(props) => props.color ? `color: ${props.theme[props.color]};` : ''}
     ${(props) => props.decoration ? `text-decoration: ${props.decoration};` : ''}
 
 `;
