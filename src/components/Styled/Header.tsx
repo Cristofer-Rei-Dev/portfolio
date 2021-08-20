a97c53f130586e7221da3e8fd721d580baf72af5
Cristofer Portela <cristoferportela@gmail.com>
2021-08-20 01:35:51 -0300

    feat: added styling to main header

--- src/components/Styled/Header.tsx
+++ src/components/Styled/Header.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export const MainHeader = styled.header`
-    
+    background-color: rgba(100, 100, 100, .01);
+    padding-bottom: 20px;
+    border-bottom: 1px solid ${(props) => props.theme.lighter};
 `;
